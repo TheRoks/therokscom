59a5b00dabfb021ef82f5dc23461dd60e57aae0f
Stefan Roks <stefanroks@gmail.com>
2020-10-11 14:08:30 +0200

    add web-vitals

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,22 @@ module.exports = {
       }
     },
     {
+      resolve: 'gatsby-plugin-web-vitals',
+      options: {
+        // The Google Analytics property ID; the reporting code won't be generated without it
+        trackingId: 'UA-20164906-1',
+        // An array with metrics you want to track and send to analytics
+        metrics: [`FID`, `TTFB`, `LCP`, `CLS`, `FCP`],
+        // Event Category (optional) { string }, default 'Web Vitals'
+        eventCategory: 'Performance',
+        // Include Web Vitals tracking in development
+        // Defaults to false meaning Vitals will only be tracked in production.
+        includeInDevelopment: false,
+        // Prints metrics in the console when true
+        debug: false,
+      }
+    },
+    {
       resolve: "gatsby-plugin-robots-txt",
       options: {
         host: "https://theroks.com",
